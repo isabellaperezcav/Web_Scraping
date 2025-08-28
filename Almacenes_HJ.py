50f7038df2119ede49feece686a21f5d324a4a66
isabellaperezcav <isabellaperezcav@gmail.com>
2025-08-28 09:21:47 -0500

    belltec sirve

--- Almacenes_HJ.py
+++ Almacenes_HJ.py
@@ -9,7 +9,7 @@ import requests
 from bs4 import BeautifulSoup
 import pandas as pd
 
-url = "https://www.mercadolibre.com.co/tienda/almacenes-hj?item_id=MCO1947417292&category_id=MCO430153&official_store_id=1960&client=recoview-selleritems&recos_listing=true"
+url = "https://www.mercadolibre.com.co/tienda/almacenes-hj"
 
 # 1. Hacer la petición
 headers = {"User-Agent": "Mozilla/5.0"}  
